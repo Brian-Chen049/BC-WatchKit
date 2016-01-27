7beea14dd22145c85fe3cf091de88c2c9515563a
chenbrian <brian.chen@gogotech.hk>
2016-01-27 11:59:11 +0800

    init

---
+++ BCWatchKit WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  BCWatchKit WatchKit Extension
+//
+//  Created by chenbrian on 1/27/16.
+//  Copyright © 2016 Brian Chen. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
